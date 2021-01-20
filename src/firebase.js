8772cf1322c4881d3d4292f6e41aaea28ec640ad
Gaurav <goenkagaurav123@gmail.com>
2021-01-20 20:55:28 +0530

    firebase_part_done

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDLlG5HJZrvpOaRyEjBoazrbfKGnNTbvJc",
+  authDomain: "challenge-db51a.firebaseapp.com",
+  projectId: "challenge-db51a",
+  storageBucket: "challenge-db51a.appspot.com",
+  messagingSenderId: "181947994552",
+  appId: "1:181947994552:web:a3e5bc8647bf88555a3f4d",
+  measurementId: "G-PESVE7VPEC"
+};
